2928b85eadf3c5746e6e4709bec3f3ed04896b08
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-08-23 14:30:55 -0400

    Beta 1.0

--- scripts/pattern-create.js
+++ scripts/pattern-create.js
@@ -1,16 +1,30 @@
 const elements = document.getElementById("list-of-elements");
+const colors = [
+  "#00308F",
+  "#004F98",
+  "#6CA0DC",
+  "#4682B4",
+  "#4B9CD3",
+  "#00308F",
+  "#004F98",
+  "#6CA0DC",
+  "#4682B4",
+  "#4B9CD3",
+];
+let index = 0;
 
 window.addEventListener("keydown", checkKeyPress, false);
 
 function checkKeyPress(key) {
   if (key.keyCode == "13") {
     elements.textContent += ",";
+    index++;
+  }
 }
-}
 
+function theFunction(x) {
+  // when locatio is clicked
 
-function theFunction(x) {// when locatio is clicked
-
-  x.style.background = "#000000";
+  x.style.background = colors[index];
   elements.textContent += " " + x.textContent;
 }
