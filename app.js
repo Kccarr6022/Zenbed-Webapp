3f1e50ce818ddc61b4f056aa49a58badfc6f0f4d
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-15 22:31:52 -0400

    node server

--- app.js
+++ app.js
@@ -3,30 +3,25 @@ const { readFileSync } = require("fs");
 
 // get all files
 const homePage = readFileSync("./index.html");
+const homeStyles = readFileSync("./styles.css");
+const homeScript = readFileSync("./scripts/pattern-create.js");
 
 const server = http.createServer((req, res) => {
   url = req.url;
   console.log(url);
 
-  // /
+  // Home
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
     res.end();
-
-    // about
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About Page</h1>");
-    res.write('<a href="/">Home</a>');
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
     res.end();
-
-    // 404
-  } else {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Error 404</h1>");
-    res.write('<a href="/">Home</a><br>');
-    res.write('<a href="/about">About</a>');
+  } else if (url === "/scripts/pattern-create.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeScript);
     res.end();
   }
 });
