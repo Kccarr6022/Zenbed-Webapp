83e97c4267d5099a5c9ec222646171d03cd4c1f7
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-18 11:17:19 -0400

    Post request data saved

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const express = require("express");
 const path = require("path");
 const { readFileSync } = require("fs");
 const app = express();
-const db = new sqlite3.Database('./zenbed.db', sqlite3.OPEN_READWRITE, (err)=>{
-  if (err) return console.error(err.message);
-  console.log('connection successful')
-})
+const db = new sqlite3.Database(
+  "./zenbed.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) return console.error(err.message);
+    console.log("connection successful");
+  }
+);
 
 // setup static and middleware
 app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
@@ -19,17 +24,20 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
   console.log("post");
+  const { patternname, sequence } = req.body;
+  console.log(patternname);
+  console.log(sequence);
   console.log(req.url);
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/db/patterns/:patternname", (req, res) => {
+  const { patternname } = req.params;
+  res.send(`${patternname}`);
 });
 
 app.listen(5000, () => {
   console.log("Listining on port 5000...");
 });
-
 
 db.close((err) => {
   if (err) return console.error(err.meesage);
