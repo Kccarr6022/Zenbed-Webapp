a4c03ec61b5340d2e9010c098d223312010c93ad
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-20 21:29:34 -0400

    working search api functionality

--- app.js
+++ app.js
@@ -17,28 +17,93 @@ app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
+  /*  Function for the home page
+   *
+   */
+
   res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
   console.log(req.url);
 });
 
 app.post("/", (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
-  console.log("post");
+  /* function for when the user submits the form
+   *
+   */
+
   const { patternname, sequence } = req.body;
-  console.log(patternname);
-  console.log(sequence);
-  console.log(req.url);
+  db.all("SELECT * from patterns", [], (err, rows) => {
+    if (err) return console.error(err.message);
+
+    const sql = `INSERT INTO patterns (id, name, sequence)
+                  VALUES(?, ?, ?)`;
+    db.run(sql, [rows.length, patternname, sequence], (err) => {
+      if (err) return console.error(err.message);
+      console.log(`Pattern ${patternname} has been added to patterns.`);
+    });
+  });
+
+  res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
 });
 
-app.get("/db/patterns/:patternname", (req, res) => {
-  const { patternname } = req.params;
-  res.send(`${patternname}`);
+app.get("/api/patterns", (req, res) => {
+  /* function for when the user requests the patterns
+   *
+   */
+  console.log(req.query);
+  const { search, limit } = req.query;
+  db.all("SELECT * from patterns", (err, rows) => {
+    if (err) {
+      res.send("No resource found");
+      return console.error(err.message);
+    }
+    if (search) {
+      const filter = rows.filter((row) => {
+        return row.name.toLowerCase().startsWith(search.toLowerCase());
+      });
+    }
+    if (limit) {
+      const rows = rows.slice(0, limit);
+    }
+    return res.status(200).json(rows);
+  });
+});
+
+app.get("/api/patterns", (req, res) => {
+  /* function for when the user requests a specific pattern
+   *
+   */
+
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts < 1) {
+    res.status(200).json({ success: true, data: [] });
+  }
+
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
   console.log("Listining on port 5000...");
 });
 
-db.close((err) => {
-  if (err) return console.error(err.meesage);
-});
+const cleanup = (event) => {
+  db.close((err) => {
+    if (err) return console.error(err.meesage);
+  });
+  console.log("database closed");
+  console.log("process closed");
+  process.exit();
+};
+
+// run cleanup function when the node process ends
+process.on("exit", cleanup);
